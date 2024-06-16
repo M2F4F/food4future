4ea9c846f393176d42d7b38f647c51380f6a9ccb
Gerrit Behrens <gerrit.n.behrens@gmail.com>
2024-06-16 14:49:15 +0200

    Added scripts to control slider values via code

--- Assets/Core/Managers/Script/Variables/VariableManager.cs
+++ Assets/Core/Managers/Script/Variables/VariableManager.cs
@@ -1,50 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System.IO;
 
 public class VariableManager : MonoBehaviour
 {
-    public float temperatureLevel;
+    public int temperatureLevel;
+    public Slider temperatureSlider;
     public float lightLevel;
-
+    public Slider lightLevelSlider;
     public TMP_Text temperatureText;
     public TMP_Text lightText;
 
     private void Start()
     {
         // Read CSV-Data
+        using (var reader = new StreamReader(""))
         // Fill data into suitable data-structure
         // Use data to calculate Score
+
+        // Set clamp for temperature slider
+        temperatureSlider.minValue = 3;
+        temperatureSlider.maxValue = 24;
+
+        // Set clmap for light level slider
+        lightLevelSlider.minValue = 0.2f;
+        lightLevelSlider.maxValue = 0.7f;
+      
         UpdateTemperatureText();
         UpdateLightText();
     }
-
-    public void SetTemperature(float value)
-    {
-        temperatureLevel = value;
-        UpdateTemperatureText();
-    }
-
     public void SetLightLevel(float value)
     {
         lightLevel = value;
         UpdateLightText();
     }
-
-    private void UpdateTemperatureText()
+    public void SetTemperature(float value)
     {
-        if (temperatureText != null)
-        {
-            temperatureText.text = temperatureLevel.ToString() + "°C";
-        }
+        temperatureLevel = (int) value;
+        UpdateTemperatureText();
     }
-
     private void UpdateLightText()
     {
         if (lightText != null)
         {
-            lightText.text = string.Format("{0:P1}", lightLevel);
+            lightText.text = string.Format("{0:P0}", lightLevel);
+        }
+    }
+    private void UpdateTemperatureText()
+    {
+        if (temperatureText != null)
+        {
+            temperatureText.text = temperatureLevel + " °C";
         }
     }
 }
