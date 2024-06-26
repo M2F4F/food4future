75280172d04a1c42b206b3ca45912e1845084f4f
Fhuu <dirobaloska@outlook.com>
2024-06-26 07:07:15 +0200

    update logo movement animatino

---
+++ Assets/Core/UI/MainMenu/Scripts/LogoAnimator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoAnimator : MonoBehaviour
+{
+    [SerializeField] private float _duration;
+    [SerializeField] private float _delay;
+    private float _screenWidth;
+    private float _screenHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _screenWidth = Screen.width;
+        _screenHeight = Screen.height;
+        StartCoroutine(MoveLogo());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator MoveLogo() {
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        yield return new WaitForSeconds(_delay);
+        float elapsedTime = 0f;
+
+        while(elapsedTime < _duration) {
+            Vector2 target = Vector2.Lerp(this.transform.localPosition, new Vector2(-(_screenWidth / 2) + rectTransform.sizeDelta.x * transform.localScale.x / 2, (_screenHeight / 2) - rectTransform.sizeDelta.y * transform.localScale.y / 2), elapsedTime / _duration);
+            this.transform.localPosition = target;
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        yield return null;
+    }
+}
