e7515d65d42dd452303bad108d4df83c96f45ebe
Fhuu <dirobaloska@outlook.com>
2024-06-27 11:33:36 +0200

    localization using scriptable object for the narative

--- Assets/Core/UI/Narative/Scripts/NarativeSlideshow.cs
+++ Assets/Core/UI/Narative/Scripts/NarativeSlideshow.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class NarativeSlideshow : MonoBehaviour
 {
-    [SerializeField] private string[] _texts;
+    [SerializeField] private LocalText[] _texts;
     [SerializeField] private TMP_Text _counter;
     private TMP_Text _textHolder;
     private int _index;
@@ -31,7 +31,7 @@ public class NarativeSlideshow : MonoBehaviour
         _index = 0;
         _textHolder = GetComponent<TMP_Text>();
         _counter.text = _index + 1 + " / " + _texts.Length;
-        _coroutine = StartCoroutine(RenderText(_texts[_index]));
+        _coroutine = StartCoroutine(RenderText(GetText(PlayerPrefs.GetString("lang"))));
     }
 
 
@@ -51,11 +51,16 @@ public class NarativeSlideshow : MonoBehaviour
         _index++;
         if(_index == _texts.Length) _index = 0;
         _counter.text = _index + 1 + " / " + _texts.Length;
-        _coroutine = StartCoroutine(RenderText(_texts[_index]));
+        _coroutine = StartCoroutine(RenderText(GetText(PlayerPrefs.GetString("lang"))));
     }
 
     private void LanguageChangeHandler(string lang) {
         if(_coroutine != null) StopCoroutine(_coroutine);
-        _coroutine = StartCoroutine(RenderText(_texts[_index]));
+        _coroutine = StartCoroutine(RenderText(GetText(lang)));
+    }
+
+    private string GetText(string lang) {
+        if(lang == "en") return _texts[_index].english;
+        return _texts[_index].deutsch;
     }
 }
