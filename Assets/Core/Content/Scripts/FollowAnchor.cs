3e94269d62ba1156e2b94496dc3a75a6326baa55
Fhuu <dirobaloska@outlook.com>
2024-07-29 23:47:26 +0200

    refactor to public variable

--- Assets/Core/Content/Scripts/FollowAnchor.cs
+++ Assets/Core/Content/Scripts/FollowAnchor.cs
@@ -6,13 +6,12 @@ using UnityEngine;
 
 public class FollowAnchor : MonoBehaviour
 {
-    private GameObject m_anchor;
+    public GameObject anchor;
     [SerializeField] private float m_offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Follow Anchor Start");
         this.transform.LookAt(GameObject.Find("Main Camera").transform);
         this.transform.eulerAngles = new Vector3(0f, transform.eulerAngles.y, 0f);
     }
@@ -20,12 +19,12 @@ public class FollowAnchor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(this.m_anchor == null) return;
-        Vector3 position = m_anchor.transform.position;
+        if(this.anchor == null) return;
+        Vector3 position = anchor.transform.position;
         transform.position = new Vector3(position.x, position.y + m_offset, position.z);
     }
 
     public void SetAnchor(string anchorName) {
-        m_anchor = GameObject.Find(anchorName);
+        anchor = GameObject.Find(anchorName);
     }
 }
